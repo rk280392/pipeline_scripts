d118a85b940fc01fd888caa89ed90fc36441a389
rajeshk <srk280392@gmail.com>
2020-01-18 19:54:59 +0530

    added script to test

---
+++ 1.py
@@ -0,0 +1,24 @@
+import pytest
+from selenium import webdriver
+import time
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support import expected_conditions as EC
+
+def test_edureka():
+    #service_log_path = "{}/firefox.log".format("/home/vagrant/jenkins/workspace/job5_5")
+    #service_args = ['--verbose']
+    #driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver", service_args=service_args,service_log_path=service_log_path)
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("--no-sandbox");
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get("http://172.31.255.16:5000/index.php")
+    driver.maximize_window()
+    link = driver.find_element_by_xpath("//a[@id='About Us']")
+    link.click()
+
