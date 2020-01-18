a04cd089be4080bd079b6482ed026c1c84530983
rajeshk <srk280392@gmail.com>
2020-01-18 09:11:25 +0530

    added other files

---
+++ installing_chrome.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+chromedriver_version="$(chromedriver --version)"
+if [[ ! $chromedriver_version ]]
+then
+	echo "Clean up existing files"
+	sudo rm -rf /usr/local/share/chromedriver
+	sudo rm -rf /usr/local/bin/chromedriver
+	sudo rm -rf /usr/bin/chromedriver
+	echo "-------Installing unzip--------"
+	sudo apt-get install unzip
+	echo "-------Fetching chromedriver--------"
+	wget -N https://chromedriver.storage.googleapis.com/79.0.3945.36/chromedriver_linux64.zip
+	echo "------unzipping chromedriver--------"
+	unzip chromedriver_linux64.zip
+	echo "-------Putttng execute permission of chromedriver---------"
+	chmod +x chromedriver
+	echo "-------moving chromer driver to /usr/local/share/----------"
+	sudo mv -f chromedriver /usr/local/share/chromedriver
+	echo "-------Creating softlink in /usr/bin and /usr/local/bin------"
+	sudo ln -s /usr/local/share/chromedriver /usr/local/bin/chromedriver
+	sudo ln -s /usr/local/share/chromedriver /usr/bin/chromedriver
+else
+	echo "chromedriver version $chromedriver_version is alreay installed"
+fi
