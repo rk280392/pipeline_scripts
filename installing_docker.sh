922a75923920100acb76e2be9f34dfd34a2d3096
rajeshk <srk280392@gmail.com>
2020-01-22 02:45:46 +0530

    reverted the docker changes

--- installing_docker.sh
+++ installing_docker.sh
@@ -2,8 +2,9 @@ echo "update"
 sudo apt-get update -y
 echo "install a few prerequisite packages which let apt use packages over HTTPS:"
 sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 7EA0A9C3F273FCD8
 echo "add the GPG key for the official Docker repository to your system"
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - >&/dev/null
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo tee apt-key add -
 echo "Add the Docker repository to APT sources:"
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 echo "update the package database with the Docker packages from the newly added repo:"
