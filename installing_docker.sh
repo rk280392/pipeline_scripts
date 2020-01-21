65d8b747253e10d1a3a80d3d4c7a3a33aa5539a9
rajeshk <srk280392@gmail.com>
2020-01-21 23:54:21 +0530

    fixed issues

--- installing_docker.sh
+++ installing_docker.sh
@@ -1,17 +1,22 @@
-mycmd="$(docker ps -aq)"
-echo "update"
-sudo apt-get update -y
-echo "install a few prerequisite packages which let apt use packages over HTTPS:"
-sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
-echo "add the GPG key for the official Docker repository to your system"
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - >&/dev/null
-echo "Add the Docker repository to APT sources:"
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-echo "update the package database with the Docker packages from the newly added repo:"
-sudo apt-get update -y
-echo "Make sure you are about to install from the Docker repo instead of the default Ubuntu repo"
-sudo apt-cache policy docker-ce
-echo "installing docker"
-sudo apt-get install docker-ce -y
+mycmd= "$(docker -v)"
+if [[ !$mycmd ]]
+then
+	echo "update"
+	sudo apt-get update -y
+	echo "install a few prerequisite packages which let apt use packages over HTTPS:"
+	sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
+	echo "add the GPG key for the official Docker repository to your system"
+	sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - >&/dev/null
+	echo "Add the Docker repository to APT sources:"
+	sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+	echo "update the package database with the Docker packages from the newly added repo:"
+	sudo apt-get update -y
+	echo "Make sure you are about to install from the Docker repo instead of the default Ubuntu repo"
+	sudo apt-cache policy docker-ce
+	echo "installing docker"
+	sudo apt-get install docker-ce -y
+else
+	echo "-------- docker was already installed-------------"
+fi
 echo "updating required packages"
 sudo bash required_package.sh
